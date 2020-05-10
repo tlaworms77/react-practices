ce56c59f9ccf8cdba1b5ce5593f99733e25200f1
kickscar <kickscar@gmail.com>
2020-05-10 13:25:49 +0900

    API 서버 작성및 통신

--- ch04/05/api-server/index.js
+++ ch04/05/api-server/index.js
@@ -2,10 +2,9 @@ import * as path from 'path';
 import express from 'express';
 import cors from 'cors';
 
-var app = express();
+const PORT = 9090;
 
-app.use(cors());
-app.use('/', express.static(path.resolve('.', 'public')));
-app.listen(9090, function() {      
-  console.log('starts seb server on port 8080');
-})
+express()
+  .use(cors())
+  .use('/', express.static(path.resolve('.', 'public')))
+  .listen(PORT, () => console.log('starts API server on port ' + PORT));
